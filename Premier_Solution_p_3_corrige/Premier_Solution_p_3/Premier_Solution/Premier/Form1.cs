fdcf0dae58e1207c2d77d1887720f72875683a77
ICAZA <icaza@veydunet.com>
2023-02-07 10:11:53 +0100

    Add files via upload

---
+++ Premier_Solution_p_3_corrige/Premier_Solution_p_3/Premier_Solution/Premier/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Premier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Bouton_Btn_Click(object sender, EventArgs e)
+        {
+            //Process.Start("https://www.google.com/search?q=" + ZoneText_Txt.Text);
+
+            Process.Start("https://www.tiktok.com/search/user?q=" + ZoneText_Txt.Text);
+        }
+    }
+}
