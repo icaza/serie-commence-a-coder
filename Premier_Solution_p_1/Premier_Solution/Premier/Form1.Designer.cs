d878704a51a58b303b8d9f131cdd754d1e524e5e
ICAZA <icaza@veydunet.com>
2023-02-07 10:06:45 +0100

    Add files via upload

---
+++ Premier_Solution_p_1/Premier_Solution/Premier/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Premier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bouton_Btn = new System.Windows.Forms.Button();
+            this.ZoneText_Txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Bouton_Btn
+            // 
+            this.Bouton_Btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Bouton_Btn.Location = new System.Drawing.Point(38, 21);
+            this.Bouton_Btn.Name = "Bouton_Btn";
+            this.Bouton_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Bouton_Btn.TabIndex = 0;
+            this.Bouton_Btn.Text = "Clique";
+            this.Bouton_Btn.UseVisualStyleBackColor = true;
+            // 
+            // ZoneText_Txt
+            // 
+            this.ZoneText_Txt.Location = new System.Drawing.Point(119, 24);
+            this.ZoneText_Txt.Name = "ZoneText_Txt";
+            this.ZoneText_Txt.Size = new System.Drawing.Size(492, 20);
+            this.ZoneText_Txt.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 68);
+            this.Controls.Add(this.ZoneText_Txt);
+            this.Controls.Add(this.Bouton_Btn);
+            this.Name = "Form1";
+            this.Text = "Premier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Bouton_Btn;
+        private System.Windows.Forms.TextBox ZoneText_Txt;
+    }
+}
+
